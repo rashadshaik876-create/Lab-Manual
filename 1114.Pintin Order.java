2b61806a8aca71a859e4fc53fb7dc9f251671e48
rashadshaik876-create <rashadshaik876@gmail.com>
2026-07-24 23:27:47 +0530

    Create 1114.Pintin Order.java

---
+++ 1114.Pintin Order.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.Semaphore;
+
+class Foo {
+
+    private Semaphore sem2 = new Semaphore(0);
+    private Semaphore sem3 = new Semaphore(0);
+
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        printFirst.run();
+        sem2.release(); 
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        sem2.acquire(); 
+        printSecond.run();
+        sem3.release(); 
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        sem3.acquire(); 
+        
+        printThird.run();
+    }
+}
